9ea7bb56cbf89fbd2e134a31e4d639d442ddeec4
sarveshwar <sarveshwar2005@gmail.com>
2024-05-09 21:10:06 +0530

    sql-lite

---
+++ User.java
@@ -0,0 +1,9 @@
+public class User {
+    private String Username;
+    private String password;
+    public User(String username, String password) {
+        this.Username = username;
+        this.password = password;
+    }
+    
+}
